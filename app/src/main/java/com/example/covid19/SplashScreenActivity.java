41370ca81f920312ee3196b75ec7e0d69107a902
G Naveen <naveengondu18@gmail.com>
2020-04-18 18:35:36 +0530

    Toolbar and No_connection status added

--- app/src/main/java/com/example/covid19/SplashScreenActivity.java
+++ app/src/main/java/com/example/covid19/SplashScreenActivity.java
@@ -8,17 +8,26 @@ import android.os.Handler;
 public class SplashScreenActivity extends Activity {
 
     public static int SPLASH_TIME=2000;
+    public static int CONNECTED=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        if(Connection.isConnected(this))
+            CONNECTED=1;
+        else
+            CONNECTED=0;
+
         new Handler().postDelayed(new Runnable(){
+            Intent i;
             @Override
             public void run() {
-
-                Intent i =new Intent(SplashScreenActivity.this, MainActivity.class);
+                if (CONNECTED==1)
+                    i =new Intent(SplashScreenActivity.this, MainActivity.class);
+                else
+                    i =new Intent(SplashScreenActivity.this, NoConnectionActivity.class);
                 startActivity(i);
                 finish();
             }
