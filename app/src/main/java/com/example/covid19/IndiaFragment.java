6e0d9cabb348f0435ec915d8b6df5bcca63efd67
G Naveen <naveengondu18@gmail.com>
2020-04-24 18:10:29 +0530

    Minor changes

--- app/src/main/java/com/example/covid19/IndiaFragment.java
+++ app/src/main/java/com/example/covid19/IndiaFragment.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -23,6 +24,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class IndiaFragment extends Fragment {
@@ -31,24 +33,24 @@ public class IndiaFragment extends Fragment {
     public IndiaFragment() {
         // Required empty public constructor
     }
-    ArrayList<String> stateCodes = new ArrayList<>();
+    List<String> stateCodes = new ArrayList<String>();
     List<String> state = new ArrayList<String>();
-    ArrayList<String> active = new ArrayList<>();
-    ArrayList<String> confirmed = new ArrayList<>();
-    ArrayList<String> deaths = new ArrayList<>();
-    ArrayList<String> recovered = new ArrayList<>();
-    ArrayList<String> deltaC = new ArrayList<>();
-    ArrayList<String> deltaD = new ArrayList<>();
-    ArrayList<String> deltaR = new ArrayList<>();
-    ArrayList<String> time = new ArrayList<>();
+    List<String> active = new ArrayList<String>();
+    List<String> confirmed = new ArrayList<String>();
+    List<String> deaths = new ArrayList<String>();
+    List<String> recovered = new ArrayList<String>();
+    List<String> deltaC = new ArrayList<String>();
+    List<String> deltaD = new ArrayList<String>();
+    List<String> deltaR = new ArrayList<String>();
+    List<String> time = new ArrayList<String>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_india, container, false);
+
+//        final TextView stats = (TextView) v.findViewById(R.id.stats);
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.spinner_item, state);
-        String [] values =
-                {"Time at Residence","Under 6 months","6-12 months","1-2 years","2-4 years","4-8 years","8-15 years","Over 15 years","Time at Residence","Under 6 months","6-12 months","1-2 years","2-4 years","4-8 years","8-15 years","Over 15 years","Time at Residence","Under 6 months","6-12 months","1-2 years","2-4 years","4-8 years","8-15 years","Over 15 years"};
 
         String mUrlString = "https://api.covid19india.org/data.json";
         StringRequest stringRequest = new StringRequest(
@@ -100,8 +102,10 @@ public class IndiaFragment extends Fragment {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.v("item", (String) parent.getItemAtPosition(position));
-                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLACK);
+                Log.v("item", parent.getItemAtPosition(position).toString());
+//                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLACK);
+//                Toast.makeText(getActivity(), parent.getItemAtPosition(position).toString(),Toast.LENGTH_LONG);
+//                stats.setText("Active : "+active.get(position));
             }
 
             @Override
